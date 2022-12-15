1a98456538e6e2861565e7c491a6b30486bd7c57
Ben Ralton <b.ralton@icloud.com>
2022-12-15 22:18:07 +0000

    :lipstick: Reworking the homepage to use an automated grid system for the cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,83 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Button } from "react-bootstrap";
+import { Button, Row, Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 
+interface Day{
+  number: number;
+  title: string;
+  description: string;
+}
+
 const Home: NextPage = () => {
+  const days:Day[] = [
+    {"number":1, "title": "Pomodoro Timer", "description":"A simple pomodoro timer with an alert when it is finished"},
+    {"number":2, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":3, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":4, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":5, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":6, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":7, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":8, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":9, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":10, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":11, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":12, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":13, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":14, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":15, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":16, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":17, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":18, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":19, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":20, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":21, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":22, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":23, "title": "Shop Front", "description":"Shopping basket of meals"},
+    {"number":24, "title": "Shop Front", "description":"Shopping basket of meals"}
+  ]
+    
+    const renderCard = (day:Day):JSX.Element => {
+      return (
+      <Card
+          bg="light"
+          key="day1"
+          text="dark"
+          style={{ width: "18rem" }}
+          className="mb-2"
+        >
+          <Card.Body>
+            <Card.Title>Day {day.number} - {day.title}</Card.Title>
+            <Card.Text>
+             {day.description}
+            </Card.Text>
+            <Button href={`/days/${day.number}`} color="primary">
+              Take a look
+            </Button>
+          </Card.Body>
+        </Card>  
+      )
+    }
+    
+   const renderRow = (daysForRow: Day[]):JSX.Element => {
+     return (
+       <Row xs='1' sm='2' md='5'>
+       {daysForRow.map((day) => <Col key={`day-${day.number}`}>{renderCard(day)}</Col>)}
+       </Row>
+     )
+   }
+   
+  const renderGrid = ():JSX.Element => { 
+    let grid = [];
+    const chunkSize = 5;
+    for (let i = 0; i < days.length; i += chunkSize) {
+      const chunk = days.slice(i, i + chunkSize);
+      grid.push(renderRow(chunk));
+    }
+    console.log("look here", grid)
+    return (<>{grid}</>);
+  }  
+  
   return (
     <div>
       <Head>
@@ -12,25 +86,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <Card
-          bg="light"
-          key="day1"
-          text="dark"
-          style={{ width: "18rem" }}
-          className="mb-2"
-        >
-          <Card.Body>
-            <Card.Title>Day 1 - Pomodoro Timer</Card.Title>
-            <Card.Text>
-              A simple pomodoro timer with an alert when it is finished.
-            </Card.Text>
-            <Button href="/days/1" color="primary">
-              Take a look
-            </Button>
-          </Card.Body>
-        </Card>
+        {renderGrid()}
       </main>
-
+      
       <footer></footer>
     </div>
   );
